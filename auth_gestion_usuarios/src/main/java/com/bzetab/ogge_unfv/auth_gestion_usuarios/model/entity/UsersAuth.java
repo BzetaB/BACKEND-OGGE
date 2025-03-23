ab37dd662824817025d89756a3e655b7013d8357
bzetab <zevallosbenjamin100@gmail.com>
2025-03-23 02:31:24 -0500

    cracion de la entidad Users_Auth

---
+++ auth_gestion_usuarios/src/main/java/com/bzetab/ogge_unfv/auth_gestion_usuarios/model/entity/UsersAuth.java
@@ -0,0 +1,32 @@
+package com.bzetab.ogge_unfv.auth_gestion_usuarios.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "USERS_AUTH")
+public class UsersAuth {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_users")
+    private Integer id_users;
+
+    @Column(name = "email_users", nullable = false, length = 100, unique = true)
+    private String email_users;
+
+    @Column(name = "password_users", nullable = false, length = 50)
+    private String password_users;
+
+    @Column(name = "status_users", nullable = false)
+    private Boolean status_users = false;
+
+    @UpdateTimestamp
+    @Column(name = "date_create_modification", nullable = false)
+    private LocalDateTime date_create_modification;
+}
